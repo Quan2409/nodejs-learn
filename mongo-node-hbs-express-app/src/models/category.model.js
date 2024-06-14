13bb5b2c6012018eeb109a6ec19cb42679313b29
Quan2409 <dokahcquan2002@gmail.com>
2024-06-14 14:57:13 +0700

    config models category and product

--- mongo-node-hbs-express-app/src/models/category.model.js
+++ mongo-node-hbs-express-app/src/models/category.model.js
@@ -1,5 +1,14 @@
 const mongoose = require("mongoose");
 
 const categorySchema = new mongoose.Schema({
-  //
+  name: {
+    type: String,
+  },
+
+  description: {
+    type: String,
+  },
 });
+
+const categoryModel = mongoose.model("categories", categorySchema);
+module.exports = categoryModel;
--- mongo-node-hbs-express-app/src/models/product.model.js
+++ mongo-node-hbs-express-app/src/models/product.model.js
@@ -1,5 +1,23 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-  //
+  name: {
+    type: String,
+  },
+
+  price: {
+    type: Number,
+  },
+
+  image: {
+    type: String,
+  },
+
+  category: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "categories",
+  },
 });
+
+const productModel = new mongoose.model("products", productSchema);
+module.exports = productModel;
